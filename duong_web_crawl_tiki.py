ff97e89ae58adbe16d769770d47dfc27ccd20b4c
Duong <duongtruongtrong@gmail.com>
2020-09-25 21:35:26 +0700

    added function get_data, and worked

--- duong_web_crawl_tiki.py
+++ duong_web_crawl_tiki.py
@@ -16,6 +16,29 @@ data_col = ['data-seller-product-id',
 'rating_percentage',
 'number_of_reviews']
 
+output_dict = {}
+for col in data_col:
+    output_dict[col] = []
+
+data_col_dict = {'general_info': ['data-seller-product-id',
+'product-sku',
+'data-title',
+'data-price',
+'data-id',
+'data-brand',
+'data-category'],
+
+'product_link': 'product_link',
+
+'product_image': 'product_image',
+
+'price':['price-regular',
+'sale-tag sale-tag-square'],
+
+'rating_percentage':'rating_percentage',
+'number_of_reviews':'number_of_reviews'
+}
+
 def get_html(link):
     """From URL return HTML code in the website.
 
@@ -47,18 +70,51 @@ def get_item_list(full_webpage_html):
 
     return item_list
 
-def get_data(item_html):
+def get_data(item_html, output_dict):
     """Get data from item_list HTML for selected data_col list.
+    Return a dictionary.
 
     get_data(item_html)
     item_html: HTML of each item in item_list, type: string
     """
 
-    pass
+    # dictionary with keys and values are list
+    d = output_dict
+    # get general_info
+    for i in data_col_dict['general_info']:
+        d[i].append(item_html[i])
+    
+    # get links
+    d['product_link'].append(item_html.a['href'])
+    d['product_image'].append(item_html.a.img['src'])
+
+    # get price
+    for i in data_col_dict['price']:
+        d[i].append(item_html.find('span', {'class':i}).text)
+    
+    # get rating_percentage
+    rating_section = item_html.find('div', {'class':'review-wrap'})
+
+    d['number_of_reviews'].append(rating_section.find('p', {'class':'review'}).text)
+
+    # getting to rating_percentage
+    p_rating = rating_section.find('p', {'class':'rating'})
+    span_rating = p_rating.find('span', {'class':'rating-content'})
+
+    d['rating_percentage'].append(span_rating.span['style'])
+
+    return d
+
+
+    
 
 tiki_link = 'https://tiki.vn/tivi/c5015?src=c.5015.hamburger_menu_fly_out_banner&page=1'
 
+# get_item_list(get_html(tiki_link))[0]
+for item_html in get_item_list(get_html(tiki_link))[:2]:
+    output_dict = get_data(item_html, output_dict)
 
+print(output_dict)
 
 # help(get_html)
 # help(get_item_list)
