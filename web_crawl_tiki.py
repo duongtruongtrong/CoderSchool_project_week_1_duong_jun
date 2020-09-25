40e8fe9b509ef5cda973d8857f53df743df15602
Duong <duongtruongtrong@gmail.com>
2020-09-25 17:14:34 +0700

    Duong create data_column.xlsx file and create function get_html

--- web_crawl_tiki.py
+++ web_crawl_tiki.py
@@ -2,9 +2,17 @@ import requests
 
 from bs4 import BeautifulSoup
 
-# Tiki - Tivi category
-r = requests.get('https://tiki.vn/tivi/c5015?src=c.5015.hamburger_menu_fly_out_banner&page=1')
+def get_html(link):
+    r = requests.get(link)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    
 
-soup = r
+number_of_product = 1
 
-stop at page that does not contain any product
+while number_of_product > 0:
+    # Tiki - Tivi category
+    r = requests.get('https://tiki.vn/tivi/c5015?src=c.5015.hamburger_menu_fly_out_banner&page=1')
+
+    soup = r
+
+    stop at page that does not contain any product
