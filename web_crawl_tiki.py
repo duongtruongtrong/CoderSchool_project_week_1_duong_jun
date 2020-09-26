65af93252dc0a212265eefb3677c0e2d9cb582b6
Jungoodboi <71629725+Jungoodboi@users.noreply.github.com>
2020-09-26 15:26:23 +0700

    Update web_crawl_tiki.py
    Provide:
    1- data-seller-product-id
    2- product-sku
    3- data-title
    4- data-price
    5- data-id
    6- data-brand
    7-data-category
    8-product_link
    9-product_image
    10-price-regular
    11-sale-tag sale-tag-square
    12-rating_percentage
    13-number_of_reviews

--- web_crawl_tiki.py
+++ web_crawl_tiki.py
@@ -1,18 +1,36 @@
 import requests
-
+r = requests.get('https://tiki.vn/tivi/c5015?src=c.5015.hamburger_menu_fly_out_banner')
+print(r.text)
 from bs4 import BeautifulSoup
+soup = BeautifulSoup(r.text, 'html.parser')
+print(soup.prettify()[:500])
+body_soup = soup.body
+print(body_soup.prettify())
+section_1_soup = body_soup.find('div', {'data-seller-product-id':'17465704'}) 
+print(section_1_soup['data-seller-product-id'])
+print(section_1_soup['data-brand'])
+print(section_1_soup['data-category'])
+print(section_1_soup['data-id'])
+print(section_1_soup['data-price'])
+print(section_1_soup['data-score'])
+print(section_1_soup['data-title'])
+print(section_1_soup['product-sku'])
+Product_link = section_1_soup.find('a')
+Product_link['href']
 
-def get_html(link):
-    r = requests.get(link)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    
+Product_image = section_1_soup.find('img')
+Product_image['src']
 
-number_of_product = 1
+section_2_soup = section_1_soup.find('span', {'class':'price-regular'})
+print(section_2_soup.text)
 
-while number_of_product > 0:
-    # Tiki - Tivi category
-    r = requests.get('https://tiki.vn/tivi/c5015?src=c.5015.hamburger_menu_fly_out_banner&page=1')
+section_3_soup = section_1_soup.find ('span', {'class':'sale-tag sale-tag-square'})
+print(section_3_soup.text)
 
-    soup = r
+gucci_2_soup = body_soup.find('p', {'class':'rating'}) 
+gucci_3_soup = gucci_2_soup.find('span', {'style':'width:92%'})
+gucci_4_soup = body_soup.find('div', {'class':'review-wrap'})
+gucci_5_soup = gucci_4_soup.find ('p', {'class':'review'})
 
-    stop at page that does not contain any product
+print(gucci_3_soup['style'])
+print(gucci_5_soup.text)
